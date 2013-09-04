5efaf2f4c2bfadaed1317b9427767480d5341e51
parroty <parroty00@gmail.com>
2013-09-05 01:46:54 +0900

    add simple logics

--- lib/coverage_sample.ex
+++ lib/coverage_sample.ex
@@ -1,2 +1,13 @@
 defmodule CoverageSample do
+  def plus(x, y) do
+    x + y
+  end
+
+  def minus(x, y) do
+    x - y
+  end
+
+  def multiply(x, y) do
+    x * y
+  end
 end
--- test/coverage_sample_test.exs
+++ test/coverage_sample_test.exs
@@ -1,7 +1,11 @@
 defmodule CoverageSampleTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert(true)
+  test "plus" do
+    assert(CoverageSample.plus(3, 5) == 8)
+  end
+
+  test "minus" do
+    assert(CoverageSample.minus(3, 5) == -2)
   end
 end
